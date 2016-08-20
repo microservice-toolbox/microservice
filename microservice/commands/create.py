3689ccc6b5ffb1c85f09841073ec53e3ee2e7888
herve <herveberaud.pro@gmail.com>
2016-08-20 22:57:51 +0200

    copy directory

--- microservice/commands/create.py
+++ microservice/commands/create.py
@@ -11,11 +11,13 @@ class ProjectBuilder:
     source = os.path.join(template, 'packaging')
     available_templates_path = os.path.join(template, 'templates')
     available_templates = []
+    user_choice = ""
+    dest = os.getcwd()
 
     def __init__(self, name):
         self.name = name
+        self.dest = os.path.join(os.getcwd(), self.name)
         self.initialize()
-        self.choose_type()
 
     def initialize(self):
         try:
@@ -31,22 +33,63 @@ class ProjectBuilder:
         except:
             fail('Loading templates failed !')
 
+        self.choose_type()
+        success("Selected template: {0}".format(self.user_choice))
+        self.setup_template()
+
     def setup_base_project(self):
-        dest = os.path.join(os.getcwd(), self.name)
         base = self.source
-        shutil.copytree(base, dest)
+        shutil.copytree(base, self.dest)
+
+    def setup_template(self):
+        base = os.path.join(self.available_templates_path,
+            self.user_choice, 'sources')
+
+        if not os.path.isdir(base):
+            fail("Source directory doesn't exist !")
+
+        to = os.path.join(self.dest, 'sources')
+        self.setup_sources(base, to)
+
+    def setup_sources(self, base, to):
+        for dirname, dirnames, filenames in os.walk(base):
+            current = None
+            for mydir in dirnames:
+                print(os.path.join(dirname, mydir))
+                shutil.copytree(os.path.join(dirname, mydir), os.path.join(to, mydir))
+
+            for myfile in filenames:
+                if os.path.basename(dirname) == 'sources':
+                    shutil.copy(os.path.join(dirname, myfile), to)
 
     def discover_available_templates(self):
         find = False
-        for dirname, dirnames, filenames in os.walk(self.available_templates_path):
+        for dirname, dirnames, filenames in os.walk(
+            self.available_templates_path):
             if not find:
                 self.available_templates = dirnames
                 return
 
-    def choose_type(self):
+    def list_templates(self):
         for index, template in enumerate(self.available_templates):
-            print("{0}. {1}".format(index, template))
-        choice = input("Choose your template : ")
+            print("{0}. {1}".format(index + 1, template))
+
+    def is_valid_choice(self, choice):
+        try:
+            choice = int(choice)
+        except ValueError:
+            return False
+
+        if choice <= 0 or choice > len(self.available_templates) :
+            return False
+        return True
+
+    def choose_type(self):
+        choice = 0
+        self.list_templates()
+        while not self.is_valid_choice(choice):
+            choice = input("Choose your template : ")
+        self.user_choice = self.available_templates[int(choice)]
 
 def run(args):
     builder = ProjectBuilder(args.to)
